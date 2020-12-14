39da73ddaa8be97aebf363273bb9bb48c4decf3b
Кичук Егор <73134697+KichukEgor@users.noreply.github.com>
2020-12-14 18:25:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function insert(num) {
+  document.form.textview.value = document.form.textview.value + num;
+}
+
+function cleanAllNum() {
+  document.form.textview.value = "";
+}
+
+function cleanLastNum() {
+  let exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0, exp.length - 1);
+}
+
+function equal() {
+  let exp = document.form.textview.value;
+  if (exp) {
+    document.form.textview.value = eval(exp);
+  }
+}
